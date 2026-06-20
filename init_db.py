7d7a1ed980d88c9158f5af53eacb76f57320aff0
Thanh Loan <thanhloan@192.168.1.5>
2026-06-20 23:52:38 +0700

    Add models package initialization

--- init_db.py
+++ init_db.py
@@ -1,15 +1,15 @@
+# main.py
 from database import Base, engine
-
-# Import tất cả model
-from models.user import User
-from models.project import Project
-from models.task import Task
-from models.label import Label
-from models.task_label import TaskLabel
+from models import User, Project, Task, Label, TaskLabel
 
 def init_db():
     Base.metadata.create_all(bind=engine)
     print("Database initialized successfully!")
 
+def main():
+    init_db()  
+    
+    print("\nchạy thành công")
+    
 if __name__ == "__main__":
-    init_db()
+    main()
